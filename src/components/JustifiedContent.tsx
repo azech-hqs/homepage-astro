5fa44117d175e1371c1a946b84f637bab7af4cf2
Alexander Zech <alexzech@gmx.de>
2023-03-20 23:21:10 -0700

    chore: add components for main body

---
+++ src/components/JustifiedContent.tsx
@@ -0,0 +1,13 @@
+import type { ParentComponent } from "solid-js";
+
+export const JustifiedContent: ParentComponent<{ class: string }> = (props) => {
+    return (
+        <div class={props.class}>
+            <div class="mx-auto max-w-7xl lg:px-8">
+                <div class="relative px-4 sm:px-8 lg:px-12">
+                    <div class="mx-auto max-w-2xl lg:max-w-5xl">{props.children}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
