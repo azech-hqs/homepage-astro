9dd0dbc00edceb0fbafede816da1abdef2f666ad
Alexander Zech <alexzech@gmx.de>
2023-03-20 23:24:02 -0700

    chore: add component to toggle theme

---
+++ src/components/ToggleTheme.tsx
@@ -0,0 +1,23 @@
+import { Component, createEffect, createSignal } from "solid-js";
+
+export const ToggleTheme: Component = () => {
+    const [theme, setTheme] = createSignal<string>(localStorage.getItem("theme") ?? "light");
+    console.log(`ToggleTheme theme: ${theme()}`);
+    function toggleTheme() {
+        console.log(`theme before toggle: ${theme()}`);
+        setTheme(theme() === "light" ? "dark" : "light");
+        console.log(`theme after toggle: ${theme()}`);
+    }
+
+    createEffect(() => {
+        if (theme() === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+        console.log(`createEffect theme: ${theme()}`);
+        localStorage.setItem("theme", theme());
+    });
+
+    return <button onClick={toggleTheme}>{theme() === "light" ? "🌙" : "🌞"}</button>;
+};
