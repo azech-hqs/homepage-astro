6cd3f03271807c735840f8ab5d0342d9aade5172
Alexander Zech <alexzech@gmx.de>
2023-11-06 14:29:02 +0100

    chore: deactivate pages 'about' 'blog'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,9 +39,7 @@ export const Navbar: Component = () => {
                         } rounded bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 md:flex`}
                     >
                         <NavBarItem title="Home" href="/" />
-                        <NavBarItem title="About" href="/about" />
                         <NavBarItem title="Publications" href="/publications" />
-                        <NavBarItem title="Blog" href="/blog" />
                     </ul>
                 </nav>
             </div>
